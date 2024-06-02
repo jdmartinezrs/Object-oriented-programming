12fc95c40e6f955fd74f040e6fd0307d01f794bf
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-06-01 20:17:24 -0500

    feat: :alembic: Learning more about encapsulation

--- js/example.js
+++ js/example.js
@@ -40,3 +40,29 @@ datosEstudiante();
 
 //la declaración global se puede llamar desde cualquier función en el codigo
 //sin embargo una variable Local solo se ejecuta dentro de si misma 
+
+
+const Persona = function (nombre, apellido,edad) {
+ var nameUser = nombre;
+ var lastNameUser = apellido;
+ var age = edad;
+
+//estos metodos podrán ser accedidos o llamados 
+
+ this.getFullName = function (){
+    return n + " " + a ;
+ };
+
+ this.getAge = function (){
+    return age;
+ };
+ this.toChangeAge = function (nuevaEdad){
+   age = nuevaEdad;
+ };
+};
+
+let persona1 = new Persona ("davidR10","Bboy", 10);
+
+
+persona1.getFullName();
+persona1.getAge();
