b6af32ea2caed24868de57c5702924518a0b9d6f
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-06-03 15:08:59 -0500

    feat: :alembic: Abstract class

--- js/argumentos.js
+++ js/argumentos.js
@@ -1,2 +1,139 @@
-//java script review
+class Empleado{
+    #nombre;
+    #dpto;
+
+    constructor(nombre,dpto){
+        this.#nombre= nombre;
+        this.#dpto=dpto;
+
+    }
+  obtenerBonificacion(){
+    throw new Error("Debe implementar funcionalidad")
+
+  }
+
+}
+class Administrativos extends Empleado {
+    obtenerbonificacion(){
+        console.log('Bonificacion 10%');
+    }
+}
+class Operaciones extends Empleado{
+    obtenerBonificacion(){
+        console.log('Bonificacion 8%');
+
+    }
+}
+class Gerentes extends Empleado{
+    obtenerBonificacion(){
+        
+
+    }
+}
+const objAdm = new Administrativos('Luisa Caceres','Tesoreria');
+objAdm.obtenerBonificacion();
+
+const objOP = new Operaciones('Pedro rivas','Maquinaria');
+objOP.obtenerBonificacion();
+
+const objGerente = new Gerentes('Leonardo Jose','Agencia principal');
+objGerente.obtenerBonificacion();
+
+const objEmpleado = new Empleado('EMpleado','Genérico');
+console.log(objEmpleado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
